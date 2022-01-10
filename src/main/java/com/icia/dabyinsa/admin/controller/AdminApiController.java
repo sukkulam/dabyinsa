dee122137a4615afd57990496f255ca532b38b96
YHB <yhb3014@gmail.com>
2022-01-10 13:40:42 +0900

    20220110_호빈_버튼

--- src/main/java/com/icia/dabyinsa/admin/controller/AdminApiController.java
+++ src/main/java/com/icia/dabyinsa/admin/controller/AdminApiController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.icia.dabyinsa.admin.dto.ResponseDto;
 import com.icia.dabyinsa.admin.service.ButtonService;
 
 @RestController
@@ -18,57 +19,57 @@ public class AdminApiController {
 
 	// 반품 버튼
 	@PutMapping("/api/returns")
-	public ResponseEntity<Integer> returns(String ocode) {
+	public ResponseDto<Integer> returns(String ocode) {
 		bs.returnsBtn(ocode);
-		return new ResponseEntity<Integer>(HttpStatus.OK);
+		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
 	}
 
 	// 환불 버튼
 	@PutMapping("/api/refund")
-	public ResponseEntity<Integer> refund(String ocode) {
+	public ResponseDto<Integer> refund(String ocode) {
 		bs.refundBtn(ocode);
-		return new ResponseEntity<Integer>(HttpStatus.OK);
+		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
 	}
 
 	// 입금 확인버튼
 	@PutMapping("/api/pOk")
-	public ResponseEntity<Integer> pOk(String ocode) {
+	public ResponseDto<Integer> pOk(String ocode) {
 		bs.pOkBtn(ocode);
-		return new ResponseEntity<Integer>(HttpStatus.OK);
+		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
 	}
 
 	// 주문취소 버튼
 	@PutMapping("/api/pCancle")
-	public ResponseEntity<Integer> pCancle(String ocode) {
+	public ResponseDto<Integer> pCancle(String ocode) {
 		bs.pCancleBtn(ocode);
-		return new ResponseEntity<Integer>(HttpStatus.OK);
+		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
 	}
 	
 	// 배송중처리 버튼
 	@PutMapping("/api/sStart")
-	public ResponseEntity<Integer> sStart(String ocode) {
+	public ResponseDto<Integer> sStart(String ocode) {
 		bs.sStartBtn(ocode);
-		return new ResponseEntity<Integer>(HttpStatus.OK);
+		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
 	}
 	
 	// 배송준비중처리 버튼
 	@PutMapping("/api/sBegin")
-	public ResponseEntity<Integer> sBegin(String ocode) {
+	public ResponseDto<Integer> sBegin(String ocode) {
 		bs.sBeginBtn(ocode);
-		return new ResponseEntity<Integer>(HttpStatus.OK);
+		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
 	}
 	
 	// 배송대기중처리 버튼
 	@PutMapping("/api/sStandBy")
-	public ResponseEntity<Integer> sStandBy(String ocode) {
+	public ResponseDto<Integer> sStandBy(String ocode) {
 		bs.sStandByBtn(ocode);
-		return new ResponseEntity<Integer>(HttpStatus.OK);
+		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
 	}
 	
 	// 배송완료처리 버튼
 	@PutMapping("/api/sEnd")
-	public ResponseEntity<Integer> sEnd(String ocode) {
+	public ResponseDto<Integer> sEnd(String ocode) {
 		bs.sEndBtn(ocode);
-		return new ResponseEntity<Integer>(HttpStatus.OK);
+		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
 	}
 }
---
+++ src/main/java/com/icia/dabyinsa/admin/dto/ResponseDto.java
@@ -0,0 +1,11 @@
+package com.icia.dabyinsa.admin.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ResponseDto<T> {
+	int status;
+	T data;
+}
