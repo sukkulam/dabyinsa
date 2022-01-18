f7fd876faff7406f86d317eb28b64f353c8fdc59
sangwon <tkddnjs1023@nate.com>
2022-01-18 14:24:38 +0900

    220118_sw_비밀번호 확인

--- src/main/java/com/icia/dabyinsa/admin/controller/AdminController.java
+++ src/main/java/com/icia/dabyinsa/admin/controller/AdminController.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
